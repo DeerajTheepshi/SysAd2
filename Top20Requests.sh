6590d385b1b230564d4e6234621c1554fb1f3af2
Deeraj Theepshi <theepshi123@gmail.com>
2018-06-09 23:00:43 +0530

    Commented

--- Top20Requests.sh
+++ Top20Requests.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 #
+#download from net
 wget https://delta.nitt.edu/~shubham/web_logs.txt
 #
+#operate on second column using awk
 awk '{print $2}' web_logs.txt | sort |uniq -c| awk '{print $2 " " $1}'|column  -t|sort -k2 -n -r|head -20
 
--- find_replace.sh
+++ find_replace.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 #
+#Read required data
 echo Enter File path: 
 read path
 #
@@ -9,5 +10,6 @@ read key
 echo Enter String to replace:
 read replace
 #
+#use Sed to replace words
 sed "s/\ $key/\ $replace/g" $path
 
